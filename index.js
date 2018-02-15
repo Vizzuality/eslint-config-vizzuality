eafa76c3f487427977bb24e7f244836e7a03cdf2
David Inga <david.inga@vizzuality.com>
2018-02-15 11:09:02 +0100

    updating rules and eslint libraries

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ module.exports = {
   ],
   rules: {
     'comma-dangle': ['error', 'never'],
-    'react/forbid-prop-types': 0,
-    'react/require-default-props': 0,
-    'no-console': ['error', {
-      'allow': ['warn', 'error', 'info']
-    }]
+    'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': : [2, { commonjs: true, amd: true }],
+    'jsx-a11y/anchor-is-valid': 2,
+    'no-console': ['error', { 'allow': ['warn', 'error', 'info'] }],
+    'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
+    'react/prefer-stateless-function': [true, { 'ignorePureComponents': true }],
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }]
   }
 };
