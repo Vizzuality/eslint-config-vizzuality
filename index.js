4d64e00ffbd336b7a75825172b254867a3bf3c24
David Inga <david.inga@vizzuality.com>
2018-02-15 11:12:45 +0100

    fixing

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ module.exports = {
   rules: {
     'comma-dangle': ['error', 'never'],
     'import/no-extraneous-dependencies': 0,
-    'import/no-unresolved': : [2, { commonjs: true, amd: true }],
+    'import/no-unresolved': [2, {
+      commonjs: true,
+      amd: true
+    }],
     'jsx-a11y/anchor-is-valid': 2,
-    'no-console': ['error', { 'allow': ['warn', 'error', 'info'] }],
-    'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
-    'react/prefer-stateless-function': [true, { 'ignorePureComponents': true }],
-    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }]
+    'no-console': ['error', {
+      'allow': ['warn', 'error', 'info']
+    }],
+    'no-plusplus': ['error', {
+      'allowForLoopAfterthoughts': true
+    }],
+    'react/prefer-stateless-function': [true, {
+      'ignorePureComponents': true
+    }],
+    'react/jsx-filename-extension': [1, {
+      'extensions': ['.js', '.jsx']
+    }]
   }
 };
