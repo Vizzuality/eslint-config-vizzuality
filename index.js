587b11f1946b70f185bda93a68534accf341865d
David Inga <david.inga@vizzuality.com>
2018-07-27 13:37:02 +0200

    fixed comma dangle rule, this is the good one

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ module.exports = {
     'prettier'
   ],
   rules: {
-    'comma-dangle': ['error', { 'only-multine': true }],
-    'function-paren-newline': ['error', 'consistent'],
-    'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
-    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-    'object-curly-newline': ['error', { multiline: true }],
-    'import/extensions': ['error', { js: 'never', jsx: 'never' }],
-    'import/no-extraneous-dependencies': ['error', {
+    'comma-dangle': [2, 'only-multiline'],
+    'function-paren-newline': [2, 'consistent'],
+    'no-console': [2, { allow: ['warn', 'error', 'info'] }],
+    'no-plusplus': [2, { allowForLoopAfterthoughts: true }],
+    'object-curly-newline': [2, { multiline: true }],
+    'import/extensions': [2, { js: 'never', jsx: 'never' }],
+    'import/no-extraneous-dependencies': [2, {
       devDependencies: false,
       optionalDependencies: false,
       peerDependencies: false,
     }],
-    'import/no-unresolved': ['error', {
+    'import/no-unresolved': [2, {
       commonjs: true,
       amd: true,
       caseSensitive: true,
     }],
-    'jsx-a11y/anchor-is-valid': 'error',
+    'jsx-a11y/anchor-is-valid': 2,
     'react/forbid-prop-types': 1,
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/prefer-stateless-function': [2, { ignorePureComponents: true }],
